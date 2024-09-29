504eca8d6b55acf2951cd9618a15c34280a17ea3
Valentyn <vvo12015@gmail.com>
2024-09-29 19:00:16 +0300

    security-user

--- src/main/java/net/vrakin/medsalary/controller/SecurityUserController.java
+++ src/main/java/net/vrakin/medsalary/controller/SecurityUserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-@RequestMapping("security-user")
+@RequestMapping("/security-user")
 public class SecurityUserController {
 
     private SecurityUserService securityUserService;
