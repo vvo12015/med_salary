459bd7a8a8817d756d02713e16bb60d8c4d6d2bf
Valentyn <vvo12015@gmail.com>
2024-10-04 00:05:22 +0300

    application.properties for db

---
+++ src/main/java/net/vrakin/medsalary/Main.java
@@ -0,0 +1,11 @@
+package net.vrakin.medsalary;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class Main {
+    public static void main(String[] args) {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("!!!&&&dpemcyd"));
+    }
+}
